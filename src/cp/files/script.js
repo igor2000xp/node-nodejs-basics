ea5e9b7db9533a7e0737c0a4e192ded6eb5f4f9c
igor2000xp <igorring@gmail.com>
2022-06-03 20:15:50 +0300

    fix: fix script.js

--- src/cp/files/script.js
+++ src/cp/files/script.js
@@ -10,36 +10,3 @@ const echoInput = (chunk) => {
 };
 
 process.stdin.on('data', echoInput);
-
-
-
-// const arguments = process.argv.slice(2);
-//
-// console.log(`Total number of arguments is ${arguments.length}`);
-// console.log(`Arguments: ${JSON.stringify(arguments)}`);
-//
-// const echoInput = (chunk) => {
-//     const chunkStringified = chunk.toString();
-//     if (chunkStringified.includes('CLOSE')) process.exit(0);
-//     process.stdout.write(`Received from master process: ${chunk.toString()}\n`)
-// };
-//
-// process.stdin.on('data', echoInput);
-
-//
-// const arguments = process.argv.slice(2);
-// const args = process.argv.slice(2);
-//
-// console.log(`Total number of arguments is ${arguments.length}`);
-// console.log(`Arguments: ${JSON.stringify(arguments)}`);
-// console.log(`Total number of arguments is ${args.length}`);
-// console.log(`Arguments: ${JSON.stringify(args)}`);
-//
-// const echoInput = (chunk) => {
-//   const chunkStringified = chunk.toString();
-//   if (chunkStringified.includes('CLOSE')) process.exit(0);
-//   process.stdout.write(`Received from master process: ${chunk.toString()}\n`)
-// };
-//
-// process.stdin.on('data', echoInput);
-// process.stdin.on('data', echoInput);
