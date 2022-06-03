e43135620b859b3e05c702596f50797232dca10f
igor2000xp <igorring@gmail.com>
2022-06-03 19:28:32 +0300

    feat: add some code to copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,9 +1,24 @@
 import * as fs from 'fs/promises';
 import * as path from 'path';
+import { fileURLToPath } from 'url';
+import { list } from './list.js';
 
-const DESTINATION_PATH = 'files_copy';
-const SOURCE_PATH = 'files';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const DESTINATION_PATH = path.resolve(__dirname, 'files_copy');
+const SOURCE_PATH = path.resolve(__dirname, 'files');
+const ERROR_FS = 'FS operation failed';
 
-export const copy = async () => {
+export const copy = async (sourcePath, destPath) => {
     // Write your code here
+   try {
+     const listFiles = list(sourcePath);
+     console.log(listFiles);
+
+   } catch (err) {
+     throw new Error(ERROR_FS);
+   }
+
 };
+
+await copy(SOURCE_PATH, DESTINATION_PATH);
