87eb19686cfe3a2bbe864b9436904b8db604e392
igor2000xp <igorring@gmail.com>
2022-06-04 14:25:24 +0300

    feat: implement cli/args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,16 @@
 export const parseArgs = () => {
-    // Write your code here 
+    // Write your code here
+  let newArr = process.argv.reduce((prev, curr, currIndex) => {
+    return currIndex > 1 ? prev + curr   + ' ' : '';
+  }).split('--').filter((el) => el !== '');
+  console.log(newArr);
+  let result = newArr.reduce((prev, curr) => {
+    curr.trim();
+    let b = curr.split(' ');
+    return `${prev.trim()}, ${b[0]} is ${b[1]}`;
+  })
+  console.log(result);
+
 };
+
+parseArgs();
