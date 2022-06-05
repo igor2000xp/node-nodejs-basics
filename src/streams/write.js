fa5369d1323fdaf7d7f4b5cb948fd0dfaeeb1b47
igor2000xp <igorring@gmail.com>
2022-06-05 20:16:53 +0300

    feat:  implement stream/transform

--- src/streams/write.js
+++ src/streams/write.js
@@ -4,11 +4,11 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const pathFileToWrite = path.resolve(__dirname, 'files', 'fileToWrite.txt');
+const PATH_FILE_TO_WRITE = path.resolve(__dirname, 'files', 'fileToWrite.txt');
 
 export const write = async () => {
     // Write your code here
-  const myStreamToWrite = fs.createWriteStream(pathFileToWrite);
+  const myStreamToWrite = fs.createWriteStream(PATH_FILE_TO_WRITE);
   const readFromTerminal = process.stdin;
 
   readFromTerminal.pipe(myStreamToWrite);
