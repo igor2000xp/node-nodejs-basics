6bc56cf91cef3d1c7a79fc7a435378f7308be73a
igor2000xp <igorring@gmail.com>
2022-06-05 23:56:14 +0300

    feat: implement draft wt/worker and main 2

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,4 +1,4 @@
-import { Worker }  from 'worker_threads';
+import { workerData, parentPort }  from 'worker_threads';
 
 let result;
 // n should be received from main thread
@@ -7,11 +7,12 @@ export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacc
 export const sendResult = () => {
     // This function sends result of nthFibonacci computations to main thread
   const status = typeof result !== 'undefined' ? 'resolved' : 'error';
-  Worker.parentPort.postMessage({status: status, data: result})
+  parentPort.postMessage({status: status, data: result})
 };
 
 function runCount() {
-  result = nthFibonacci(Worker.workerData);
+  console.log(workerData);
+  result = nthFibonacci(workerData);
   sendResult()
 }
 
