bf6d163379bc8e05ae4c3c69528c248672d6076b
igor2000xp <igorring@gmail.com>
2022-06-05 19:25:45 +0300

    feat:  implement stream/write

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,17 @@
+import * as fs from 'fs';
+import * as path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const pathFileToWrite = path.resolve(__dirname, 'files', 'fileToWrite.txt');
+
 export const write = async () => {
-    // Write your code here 
+    // Write your code here
+  const myStreamToWrite = fs.createWriteStream(pathFileToWrite);
+  const readFromTerminal = process.stdin;
+
+  readFromTerminal.pipe(myStreamToWrite);
 };
+
+await write();
